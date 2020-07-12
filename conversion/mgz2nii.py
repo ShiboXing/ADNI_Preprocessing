64ab52478e84e4bedfaabef9cae113724e062338
Shibo Xing <shx26@pitt.edu>
2020-07-12 02:04:21 -0400

    feat: add empty space clipping

--- conversion/mgz2nii.py
+++ conversion/mgz2nii.py
@@ -1,5 +1,7 @@
+%pylab inline
 import argparse as argp
 import os 
+import matplotlib.pyplot as plt
 
 def dive(root):
     print(root)
@@ -7,12 +9,21 @@ def dive(root):
         path = rf'{root}/{f}'
         print(path)
         if f.endswith('.mgz'):
-            convert(root, f)
+            nii_path = convert(root, f)
+            clip_space(nii_path)
         elif os.path.isdir(path):
             dive(path)
             
 def convert(root, f):
     os.system(rf'mri_convert {root}/{f} {root}/image.nii')
+    return rf'mri_convert {root}/{f} {root}/image.nii'
+
+# remove the surrounding zero-intensity space from the brain
+def clip_boundary(path):
+    img = nib.load(path)
+    fig, axes = plt.subplots(1, len(slices))
+    slice_0 = epi_img_data[26, :, :]
+    axes[0].imshow(slice.T, cmap="gray", origin="lower")
 
 if __name__ == '__main__':
     parser = argp.ArgumentParser(description='convert .mgz files to .nii')
