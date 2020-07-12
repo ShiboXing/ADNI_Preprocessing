46a81c7bab6c5ca5aa6304f8ab089e2a6abad531
Shibo Xing <yakuza@STANLEYXIN-PCAO.localdomain>
2020-07-12 20:51:31 +0800

    feat: complete clean space function

--- conversion/mgz2nii.py
+++ conversion/mgz2nii.py
@@ -1,7 +1,5 @@
-%pylab inline
 import argparse as argp
 import os 
-import matplotlib.pyplot as plt
 
 def dive(root):
     print(root)
@@ -9,21 +7,17 @@ def dive(root):
         path = rf'{root}/{f}'
         print(path)
         if f.endswith('.mgz'):
+<<<<<<< HEAD
             nii_path = convert(root, f)
-            clip_space(nii_path)
+#             clip_space(nii_path)
+=======
+            convert(root, f)
+>>>>>>> parent of 64ab524... feat: add empty space clipping
         elif os.path.isdir(path):
             dive(path)
             
 def convert(root, f):
     os.system(rf'mri_convert {root}/{f} {root}/image.nii')
-    return rf'mri_convert {root}/{f} {root}/image.nii'
-
-# remove the surrounding zero-intensity space from the brain
-def clip_boundary(path):
-    img = nib.load(path)
-    fig, axes = plt.subplots(1, len(slices))
-    slice_0 = epi_img_data[26, :, :]
-    axes[0].imshow(slice.T, cmap="gray", origin="lower")
 
 if __name__ == '__main__':
     parser = argp.ArgumentParser(description='convert .mgz files to .nii')
---
+++ test.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function test() {
+    const test = 'test';
+    if (/test/.test('testmyass'))
+        return 1;
+    var func = () => {
+        () => {
+            const func3 = function() {
+                
+            }
+        }
+    }
+}
